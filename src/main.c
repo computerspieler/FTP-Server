f8c102913bc1eb4a27e3405c5470d738bfe18e83
computerspieler <roann.cantel@gmail.com>
2020-09-24 19:56:23 +0200

    Add a Makefile, a test file and a .gitignore

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "typedef.h"
+
+int main(int argc, char* argv[])
+{
+	printf("It works !\n");
+	return 0;
+}
---
+++ src/typedef.h
@@ -0,0 +1,8 @@
+#ifndef _TYPEDEF_H_
+#define _TYPEDEF_H_
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+
+#endif
